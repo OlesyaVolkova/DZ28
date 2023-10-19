a6beb685f9a7f7222a9d8d71b046a7ffedafe197
Lesya <olesyashka@work.ru>
2023-10-19 11:36:07 +0400

    FileReader

---
+++ src/Teacher.java
@@ -0,0 +1,31 @@
+public class Teacher extends Human{
+    private String spesiality;
+    private int experience;
+
+    public String getSpesiality() {
+        return spesiality;
+    }
+
+    public int getExperience() {
+        return experience;
+    }
+
+    public void setSpesiality(String spesiality) {
+        this.spesiality = spesiality;
+    }
+
+    public void setExperience(int experience) {
+        this.experience = experience;
+    }
+
+    public Teacher(String lastName, String firstName, int age, String spesiality, int experience) {
+        super(lastName, firstName, age);
+        setSpesiality(spesiality);
+        setExperience(experience);
+        System.out.println("TConstructor:\t" + Integer.toHexString(hashCode()));
+    }
+
+    public String toString(){
+        return  super.toString() + " " + spesiality + " " + experience;
+    }
+}
